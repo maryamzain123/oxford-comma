c95c3ded29f285160a4e790d433dd79d267017cf
maryamzain123 <smaryam.zain@gmail.com>
2021-02-25 20:17:47 -0600

    commit tree 2bb1221097b9c4c9b7363822b0d6e39337010287

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
 
-end
+def oxford_comma(array)
+    if array.size >= 3 
+      last = array.pop
+      array.push('and '+last)
+      array.join(', ')
+    elsif array.size >= 1
+      array.join(' and ')  
+    else
+      array.join  
+    end  
+  end
